f2169b9ab5d8e68f2f5d41a0925ca4c36a53b114
Ruan Miguel <ruanmiguelps@gmail.com>
2021-08-16 14:17:06 -0300

    Ruan Miguel Project-Job-Insights

--- src/jobs.py
+++ src/jobs.py
@@ -4,15 +4,15 @@ from functools import lru_cache
 @lru_cache
 def read(path):
     """Reads a file from a given path and returns its contents
+    q
+        Parameters
+        ----------
+        path : str
+            Full path to file
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
+        Returns
+        -------
+        list
+            List of rows as dicts
     """
     return []
